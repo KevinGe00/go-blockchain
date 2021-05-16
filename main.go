991144db0aace50aa691393b8d9997d32ea10b88
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-15 22:30:49 -0400

    added blockchain validity check function

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 var blockChain []Block
 
 type Block struct {
+	index int
 	hash string
 	prevHash string
 	data string
@@ -30,28 +31,64 @@ func getLastBlock () Block{
 	return blockChain[len(blockChain) - 1]
 }
 
-func addNewBlock (data string) {
-	var oldBlockHash string
-	if len(blockChain) > 0 {
-		oldBlock := getLastBlock()
-		oldBlockHash = oldBlock.hash
-	} else {
-		// Genesis block has no prev hash
-		oldBlockHash = ""
-	}
-	
+func createNewBlock (index int, prevHash, data string) Block{
 	newBlock := Block{
-		prevHash: oldBlockHash,
+		index: index,
+		prevHash: prevHash,
 		data: data,
 		timeStamp: time.Now().String(),
 	}
 	newBlock.hash = calculateHash(newBlock)
 
+	return newBlock
+}
+
+func addNewBlock (data string) {
+	var lastBlockHash string
+	var index int
+	if len(blockChain) > 0 {
+		lastBlock := getLastBlock()
+		lastBlockHash = lastBlock.hash
+		index = lastBlock.index + 1
+	} else {
+		// Genesis block has no prev hash
+		lastBlockHash = ""
+		index = 0
+	}
+
+	newBlock := createNewBlock(index, lastBlockHash, data)
 	blockChain = append(blockChain, newBlock)
+}
+
+// Checks if anyone has tried tampering with the blockchain
+func isBlockChainValid() bool {
+	for i, currBlock := range blockChain {
+    	if (i > 0 ) { // Skip first block
+			prevBlock := blockChain[i - 1]
+			if (currBlock.index != prevBlock.index + 1) {
+				return false
+			}
+			if (currBlock.prevHash != prevBlock.hash) {
+				return false
+			}
+			if (calculateHash(currBlock) != currBlock.hash) {
+				return false
+			}
+		} 
+    }
+	return true
 }
 
 func main () {
 	addNewBlock("Genesis block")
 	addNewBlock("2nd block")
+	addNewBlock("3rd block")
 	spew.Dump(blockChain)
+	
+	spew.Dump(isBlockChainValid())
+
+	blockChain[1].data = "I've messed with the blockchain"
+	blockChain[1].hash = calculateHash(blockChain[1])
+
+	spew.Dump(isBlockChainValid())
 }
