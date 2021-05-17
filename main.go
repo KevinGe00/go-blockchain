211caa4aec96b72fcb1740e4b658a29ca6ff0114
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-16 22:05:16 -0400

    added mechanism to require that new blocks added to blockchain must be mined/have proof of work

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"strings"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
 )
 
 var blockChain []Block
+const difficulty int = 4 // Changes how long it takes to mine one new block
 
 type Block struct {
 	index int
@@ -16,12 +18,13 @@ type Block struct {
 	prevHash string
 	data string
 	timeStamp string
+	nonce int
 }
 
 // Generating a digital fingerprint of a block
 func calculateHash (block Block) string{
 	h := sha256.New()
-	unique := block.data + block.prevHash + block.timeStamp
+	unique := block.data + block.prevHash + block.timeStamp + string(block.nonce)
 	h.Write([]byte(unique))
 	
 	return hex.EncodeToString(h.Sum(nil))
@@ -37,10 +40,23 @@ func createNewBlock (index int, prevHash, data string) Block{
 		prevHash: prevHash,
 		data: data,
 		timeStamp: time.Now().String(),
+		nonce: 0,
 	}
 	newBlock.hash = calculateHash(newBlock)
 
 	return newBlock
+}
+
+// Proof of work algorithm: keep "mining" until we find a 
+// nonce that makes our calculate hash satisfy the number
+// of leading zeroes required by "difficulty".
+func mineNewBlock (block *Block) {
+	proofPrefix := strings.Repeat("0", difficulty)
+	for calculateHash(*block)[:difficulty] != proofPrefix {
+		block.nonce ++
+	}
+
+	block.hash = calculateHash(*block)
 }
 
 func addNewBlock (data string) {
@@ -55,13 +71,17 @@ func addNewBlock (data string) {
 		lastBlockHash = ""
 		index = 0
 	}
-
 	newBlock := createNewBlock(index, lastBlockHash, data)
+
+	// Only once our new block has proof-of-work through mining do we add it to the blockchain
+	mineNewBlock(&newBlock)
 	blockChain = append(blockChain, newBlock)
 }
 
 // Checks if anyone has tried tampering with the blockchain
 func isBlockChainValid() bool {
+	proofPrefix := strings.Repeat("0", difficulty)
+
 	for i, currBlock := range blockChain {
     	if (i > 0 ) { // Skip first block
 			prevBlock := blockChain[i - 1]
@@ -72,6 +92,9 @@ func isBlockChainValid() bool {
 				return false
 			}
 			if (calculateHash(currBlock) != currBlock.hash) {
+				return false
+			}
+			if (currBlock.hash[:difficulty] != proofPrefix) {
 				return false
 			}
 		} 
