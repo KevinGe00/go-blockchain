e1b6a2c80716881ba8f64d3dde0dcb25077437c6
Kevin Ge <kevinge@Garys-MacBook-Air.local>
2021-05-17 17:00:25 -0400

    Initialized a web server to make our services available as an API, only implemented get functions

--- go.mod
+++ go.mod
@@ -4,4 +4,5 @@ go 1.16
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,28 +3,34 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/gorilla/mux"
 )
 
 var blockChain []Block
 const difficulty int = 4 // Changes how long it takes to mine one new block
 
 type Block struct {
-	index int
-	hash string
-	prevHash string
-	data string
-	timeStamp string
-	nonce int
+	Index int
+	Hash string
+	PrevHash string
+	Data string
+	TimeStamp string
+	Nonce int
 }
 
 // Generating a digital fingerprint of a block
 func calculateHash (block Block) string{
 	h := sha256.New()
-	unique := block.data + block.prevHash + block.timeStamp + string(block.nonce)
+	unique := block.Data + block.PrevHash + block.TimeStamp + string(block.Nonce)
 	h.Write([]byte(unique))
 	
 	return hex.EncodeToString(h.Sum(nil))
@@ -36,13 +42,13 @@ func getLastBlock () Block{
 
 func createNewBlock (index int, prevHash, data string) Block{
 	newBlock := Block{
-		index: index,
-		prevHash: prevHash,
-		data: data,
-		timeStamp: time.Now().String(),
-		nonce: 0,
+		Index: index,
+		PrevHash: prevHash,
+		Data: data,
+		TimeStamp: time.Now().String(),
+		Nonce: 0,
 	}
-	newBlock.hash = calculateHash(newBlock)
+	newBlock.Hash = calculateHash(newBlock)
 
 	return newBlock
 }
@@ -53,10 +59,10 @@ func createNewBlock (index int, prevHash, data string) Block{
 func mineNewBlock (block *Block) {
 	proofPrefix := strings.Repeat("0", difficulty)
 	for calculateHash(*block)[:difficulty] != proofPrefix {
-		block.nonce ++
+		block.Nonce ++
 	}
 
-	block.hash = calculateHash(*block)
+	block.Hash = calculateHash(*block)
 }
 
 func addNewBlock (data string) {
@@ -64,8 +70,8 @@ func addNewBlock (data string) {
 	var index int
 	if len(blockChain) > 0 {
 		lastBlock := getLastBlock()
-		lastBlockHash = lastBlock.hash
-		index = lastBlock.index + 1
+		lastBlockHash = lastBlock.Hash
+		index = lastBlock.Index + 1
 	} else {
 		// Genesis block has no prev hash
 		lastBlockHash = ""
@@ -85,21 +91,50 @@ func isBlockChainValid() bool {
 	for i, currBlock := range blockChain {
     	if (i > 0 ) { // Skip first block
 			prevBlock := blockChain[i - 1]
-			if (currBlock.index != prevBlock.index + 1) {
+			if (currBlock.Index != prevBlock.Index + 1) {
 				return false
 			}
-			if (currBlock.prevHash != prevBlock.hash) {
+			if (currBlock.PrevHash != prevBlock.Hash) {
 				return false
 			}
-			if (calculateHash(currBlock) != currBlock.hash) {
+			if (calculateHash(currBlock) != currBlock.Hash) {
 				return false
 			}
-			if (currBlock.hash[:difficulty] != proofPrefix) {
+			if (currBlock.Hash[:difficulty] != proofPrefix) {
 				return false
 			}
 		} 
     }
 	return true
+}
+
+
+// Blockchain as an API
+func handleRequests() {
+	r := mux.NewRouter()
+
+	// Paths
+    r.HandleFunc("/blockchain", getBlockchainHandler)
+	r.HandleFunc("/blockchain/{index}", getBlockHandler)
+
+	log.Fatal(http.ListenAndServe(":10000", r))
+}
+
+// View entire blockchain
+func getBlockchainHandler(w http.ResponseWriter, r *http.Request){
+    json.NewEncoder(w).Encode(blockChain)
+    fmt.Println("Endpoint Hit: /blockchain")
+}
+
+// View single block on blockchain
+func getBlockHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+    i, _ := strconv.Atoi(vars["index"])
+	if (i < 0 || len(blockChain) <= i) {
+		fmt.Fprintf(w, "Index out of range")
+		return
+	} 
+	json.NewEncoder(w).Encode(blockChain[i])
 }
 
 func main () {
@@ -108,10 +143,9 @@ func main () {
 	addNewBlock("3rd block")
 	spew.Dump(blockChain)
 	
-	spew.Dump(isBlockChainValid())
+	spew.Dump(isBlockChainValid())	// True
+	blockChain[1].Data = "I've messed with the blockchain"
+	spew.Dump(isBlockChainValid()) // False
 
-	blockChain[1].data = "I've messed with the blockchain"
-	blockChain[1].hash = calculateHash(blockChain[1])
-
-	spew.Dump(isBlockChainValid())
+	handleRequests()
 }
